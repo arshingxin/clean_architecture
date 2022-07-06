08ea4d06a6f4f79beb73ec212d7a5dfbdcd5654d
star.liu <arshingxin@gmail.com>
2022-07-06 12:19:47 +0800

    我的layout

--- app/src/main/java/com/star/cla/ui/my/MyFragment.kt
+++ app/src/main/java/com/star/cla/ui/my/MyFragment.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.star.cla.databinding.FragmentMyBinding
-import com.star.extension.observe
 
 class MyFragment : Fragment() {
 
@@ -29,10 +27,6 @@ class MyFragment : Fragment() {
         _binding = FragmentMyBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textMy
-        observe(myViewModel.text) {
-            textView.text = it
-        }
         return root
     }
 
